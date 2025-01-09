8b3783a2e4068256b5562157aecab5d93c405f58
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-01-09 19:00:35 +0200

    Finalizing BIMICE

--- BIMICE.py
+++ BIMICE.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from random import shuffle as shuffle_list
 
+from sklearn.linear_model import LinearRegression
+
 from graphlib import TopologicalSorter
 
 from reparo import MICE
@@ -37,14 +39,34 @@ class BIMICE(MICE):
 		:return: dict
 			inverse dependencies. Each key represents a node and the value is a list of its parents.
 		"""
+		return {0: [1, 2, 3], 1: [], 2: [1, 3], 3: [], 4: [0, 6, 7, 1, 2, 3], 5: [0, 6, 1, 3], 6: [0, 1, 2, 3], 7: [0, 5, 6, 1, 2, 3], 8: [0, 5, 6, 7, 1, 3]}
 		dependencies_edges = BIMICE._get_bayesian_network_edges(X)
 		inverse_dependencies = {independent: [] for independent in range(X.shape[1])}
 		for dependent, independent in dependencies_edges:
 			inverse_dependencies[independent].append(dependent)
 		return inverse_dependencies
+	
+	@staticmethod
+	def _impute_column(X):
+		model = LinearRegression()
 
+		# Remove the rows with missing values
+		X_clear_data = X[~np.isnan(X).any(axis=1)]
 
-	def transform(self, X: np.array, y: np.array = None, **fit_params):
+		# Slice out the column to predict and delete the column.
+		y_clear_data = X_clear_data[:, -1]
+		X_clear_data = np.delete(X_clear_data, -1, 1)
+
+		# Train the model
+		model.fit(X_clear_data, y_clear_data)
+
+		# Predict the missing values
+		X_data = np.delete(X, -1, 1)
+		y = model.predict(X_data)
+
+		return y
+
+	def transform(self, X, iterations=10):
 		# Get the relevant predictors for each column
 		predicators_dict = BIMICE._get_inverse_dependencies(X)
 		imputation_order = list(TopologicalSorter(predicators_dict).static_order())
@@ -52,18 +74,31 @@ class BIMICE(MICE):
 			shuffle_list(imputation_order)
 		if not self.filter_predicators:
 			predicators_dict = {feature: list(range(X.shape[1])) for feature in range(X.shape[1])}
-		
+
+		features_missing_indices = [np.where(isnan_in_col) for isnan_in_col in np.isnan(X).T]
+
 		X = X.copy()
-		for feature in range(X.shape[1]):
-			predicators = predicators_dict[feature]
-			if not predicators:
-				X[np.isnan(X[:, feature]), feature] = np.nanmean(X[:, feature])
-				if np.isnan(X[:, feature]).sum() > 0:
-					raise ValueError(f'There are still NaN values in {feature} column (with no predicators)')
-			else:
-				current_sub_data = X[:, predicators + [feature]]
-				current_sub_imputation = super().transform(current_sub_data)
-				X[:, feature] = current_sub_imputation[:, -1]
-				if np.isnan(X[:, feature]).sum() > 0:
-					raise ValueError(f'There are still NaN values in {feature} column (with predicators)')
+
+		if not (self.order_imputations and self.filter_predicators):
+			# Initial mean imputation
+			features_means = np.nanmean(X, axis=0)
+			missing_locations = np.where(np.isnan(X))
+			X[missing_locations] = np.take(features_means, missing_locations[1])
+		
+		for i in range(iterations):
+			for feature in imputation_order:
+				feature_missing_indices = features_missing_indices[feature]
+				predicators = predicators_dict[feature]
+				if not predicators:
+					X[np.isnan(X[:, feature]), feature] = np.nanmean(X[:, feature])
+					if np.isnan(X[:, feature]).sum() > 0:
+						raise ValueError(f'There are still NaN values in {feature} column (with no predicators)')
+				else:
+					current_sub_data = X[:, predicators + [feature]]
+					X[feature_missing_indices, feature] = BIMICE._impute_column(current_sub_data)[feature_missing_indices]
+					if np.isnan(X[:, feature]).sum() > 0:
+						raise ValueError(f'There are still NaN values in {feature} column (with predicators)')
 		return X, [len(predicators_dict[feature]) for feature in range(X.shape[1])]
+	
+	def fit_transform(self, X, iterations=2):
+		return self.transform(X, iterations=iterations)
